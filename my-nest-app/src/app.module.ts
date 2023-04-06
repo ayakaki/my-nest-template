ebc733e559f2a332e7f5b943a4e6369612d904a1
柿澤 彩人 <ayato.kakizawa@emology.co.jp>
2023-04-06 11:30:13 +0900

    Refactor database configuration to separate file

--- my-nest-app/src/app.module.ts
+++ my-nest-app/src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user.module';
 import { join } from 'path';
+import { TypeOrmConfigService } from './infrastructure/orm/typeorm.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+      load: [],
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
@@ -16,25 +21,9 @@ import { join } from 'path';
       context: ({ req }) => ({ req }),
       installSubscriptionHandlers: true,
     }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [`.env.${process.env.NODE_ENV}`],
-      load: [],
-    }),
     UserModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'user-graphql-trial',
-      password: 'P_ssw0rd',
-      database: 'graphql_trial_db',
-      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-      synchronize: true,
-      logging: false
-    }),
+    TypeOrmModule.forRootAsync(TypeOrmConfigService),
   ],
-  providers: [AppService],
 })
 
 export class AppModule {}
---
+++ my-nest-app/src/infrastructure/orm/typeorm.config.ts
@@ -0,0 +1,17 @@
+// typeorm.config.ts
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+
+export const TypeOrmConfigService: TypeOrmModuleAsyncOptions = {
+  useFactory: async (configService: ConfigService) => ({
+    type: 'mysql',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT') || 3306,
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    entities: [join(__dirname, '..', '..', '**', '*.entity.{ts,js}')],
+  }),
+  inject: [ConfigService],
+};
